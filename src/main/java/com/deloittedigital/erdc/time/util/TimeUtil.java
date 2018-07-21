c8c40fd42e49c000f8672715bce1499dc8b26b16
Bogdan Stefan <bstefan@deloittece.com>
2018-07-21 20:44:59 +0300

    Refactored WeeksInfo: weeks and days are now long. Added correct JUnit 5 dependency. Javadoc improvements. Initial implementation of TimeUtil#weeksBetween method. Added initial tests for TimeUtil.

--- src/main/java/com/deloittedigital/erdc/time/util/TimeUtil.java
+++ src/main/java/com/deloittedigital/erdc/time/util/TimeUtil.java
@@ -1,6 +1,8 @@
 package com.deloittedigital.erdc.time.util;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.Objects;
 
 /**
  * An utility class that provides helper methods for interacting with time periods.
@@ -17,14 +19,24 @@ public class TimeUtil {
      *
      * @param startDate indicating the beginning date of the time interval. Not {@code null}
      * @param endDate   a later date, indicating the end of the interval. Not {@code null}, equal or after {@code startDate}
-     *
      * @return an object which indicates how many weeks (and possibly extra days) are between the two dates
-     *
      * @see WeeksInfo
      */
     public static WeeksInfo weeksBetween(LocalDate startDate, LocalDate endDate) {
-        // TODO Implement me
-        throw new UnsupportedOperationException("Not yet implemented.");
+        // Sanity checks:
+        Objects.requireNonNull(startDate);
+        Objects.requireNonNull(endDate);
+        if (startDate.isAfter(endDate)) {
+            throw new IllegalArgumentException("Start Date cannot be after End Date!");
+        }
+
+        final Period between = Period.between(startDate, endDate);
+        final long asDays = (between.toTotalMonths() * 31) + between.getDays()
+                /* Account for exclusion of endDate day in Period: */ +  1;
+        final long fullWeeks = asDays / 7;
+        final long leftoverDays = asDays % 7;
+
+        return new WeeksInfo(startDate, endDate, fullWeeks, leftoverDays);
     }
 
     /**
@@ -35,9 +47,9 @@ public class TimeUtil {
      */
     public static final class WeeksInfo {
 
-        private final int weeks;
-        private final int extraDays;
-        private final int totalDays;
+        private final long weeks;
+        private final long extraDays;
+        private final long totalDays;
         private final LocalDate startDate;
         private final LocalDate endDate;
 
@@ -50,7 +62,7 @@ public class TimeUtil {
          * @param weeks     indicates the number of weeks
          * @param extraDays a number between 0-6 accounting for leftover days in an incomplete week
          */
-        public WeeksInfo(LocalDate startDate, LocalDate endDate, int weeks, int extraDays) {
+        public WeeksInfo(LocalDate startDate, LocalDate endDate, long weeks, long extraDays) {
             this.weeks = weeks;
             this.extraDays = extraDays;
             this.totalDays = (weeks * 7) + extraDays;
@@ -63,7 +75,7 @@ public class TimeUtil {
          *
          * @return the number of weeks, >= 0
          */
-        public int getWeeks() {
+        public long getWeeks() {
 
             return weeks;
         }
@@ -73,7 +85,7 @@ public class TimeUtil {
          *
          * @return the number of leftover days; between 0 and 6.
          */
-        public int getExtraDays() {
+        public long getExtraDays() {
 
             return extraDays;
         }
@@ -98,11 +110,11 @@ public class TimeUtil {
         }
 
         /**
-         * Returns the number of weeks, and any leftover days, between two input dates <em>as total days</em>.
+         * Returns the number of weeks, and any leftover days, between two input dates <em>as (normalized) total days</em>.
          *
          * @return the total number of days between two dates.
          */
-        public int asDays() {
+        public long asDays() {
 
             return totalDays;
         }
